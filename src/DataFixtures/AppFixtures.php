e05e20d703ee88d6ce1f5d92c07ba18e4b75b34d
AlexAnthony13003 <alexisguillermain.pro@gmail.com>
2023-05-15 00:21:58 +0200

    🚧Début de Recette

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ingredient;
+use App\Entity\Recette;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,11 +12,32 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        //Ingrédients
+        $ingredients = [];
         for ($i=1; $i < 50; $i++) { 
-            $ingredient = new Ingredient;
+            $ingredient = new Ingredient();
             $ingredient->setName('Ingredient ' . $i)
             ->setPrice(mt_rand(0,100));
+
+            $ingredients[] = $ingredient;
             $manager->persist($ingredient);      
+        }
+
+        //Recettes
+        for ($j=1; $j < 25; $j++) { 
+            $recette = new Recette();
+            $recette->setName('Recette ' . $j)
+            ->setTime(mt_rand(0,1) == 1 ? mt_rand(1, 60) : null)
+            ->setNumberOfPersons(mt_rand(0,1) == 1 ? mt_rand(1, 12) : null)
+            ->setDifficulty(mt_rand(0,1) == 1 ? mt_rand(1, 5) : null)
+            ->setDescription('lalalalalalalalala'. $j)
+            ->setPrice(mt_rand(1, 100))
+            ->setIsFavorite(mt_rand(0,1) == 1 ? true : false);
+
+            for ($k=0; $k <mt_rand(5, 15) ; $k++) { 
+                $recette->addIngredient($ingredients[mt_rand(0, count($ingredients) -1)]);
+            }
+            $manager->persist($recette);      
         }
         $manager->flush();
     }
